7cdeef3dac94a37ac6def22d27685f0c075b6648
Thomas Porcaro <thomas.porcaro@gmail.com>
2022-10-18 22:04:22 +1100

    add samples converted to csv, code to produce csv versions of samples

--- oepr/sample.py
+++ oepr/sample.py
@@ -2,6 +2,8 @@
 """
 Sample.
 """
+import csv
+import functools
 import os.path
 import pathlib
 import sys
@@ -13,12 +15,17 @@ import oepr.preprocess
 
 SAMPLE_FUNCTION = oepr.preprocess.sample_average_bucketed_centroid_distance
 
+
 def config_parser(subp):
     parse = subp.add_parser('sample')
     parse.set_defaults(func=oepr.sample.main)
-    parse.add_argument('--read', action='store_true',
-                          help='read samples to stdout')
+    grp = parse.add_mutually_exclusive_group()
+    grp.add_argument('--read', action='store_true',
+                     help='read samples to stdout')
+    grp.add_argument('--convert', action='store_true',
+                     help='convert shelf files to csv')
     return parse
+
 
 def key(name_take):
     return name_take.replace(' ', '_')
@@ -27,6 +34,34 @@ def key(name_take):
 def read(path_cfg):
     for s in oepr.db.kv_read_all(path_cfg, 'sample'):
         print(s)
+
+    return getattr(os, 'EX_OK', 0)
+
+
+def convert(path_cfg):
+    path_samples = oepr.settings.get_cfg_field('path_samples', path_cfg)
+
+    titlerow = functools.reduce(
+        lambda a, b: a + b,
+        ('X.{0} Y.{0} Z.{0}'.format(k).split() for k in range(1, 89)),
+        ['X', 'Y', 'Z']
+    )
+
+    def frame_to_row(frame):
+        return functools.reduce(lambda a, b: a + list(b), frame['points'], [])
+
+    for s in oepr.db.kv_read_all(path_cfg, 'sample'):
+        metadata = s.pop('metadata')
+        sample = s.pop('sample')
+
+        path = os.path.join(path_samples, '%s.csv' % key(metadata['Take Name']))
+
+        with open(path, 'w', newline='') as hdl:
+            w = csv.writer(hdl, delimiter=',')
+
+            w.writerow(titlerow)
+            for frame in sample:
+                w.writerow(frame_to_row(frame))
 
     return getattr(os, 'EX_OK', 0)
 
@@ -50,6 +85,8 @@ def main(args):
 
     if args.read:
         return read(path_cfg)
+    elif args.convert:
+        return convert(path_cfg)
 
     for c in oepr.read.get_csvs(path_labelled_visits):
         try:
